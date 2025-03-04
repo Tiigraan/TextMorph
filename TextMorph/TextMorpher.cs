994d7868748cd8b3672c15a5cc409f5fa31b09ba
Tigran <davidyan.tigran.git@gmail.com>
2025-03-04 22:41:16 +0300

    Initial commit

---
+++ TextMorph/TextMorpher.cs
@@ -0,0 +1,10 @@
+namespace TextMorph;
+
+public class TextMorpher
+{
+    public void AddPair(string key, string value)
+        => throw new NotImplementedException();
+
+    public string Morph(string text)
+        => throw new NotImplementedException();
+}
