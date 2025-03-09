488e462810aaed37140997b433bfee9d823b1576
Tigran <davidyan.tigran.git@gmail.com>
2025-03-10 00:17:39 +0300

    feat(textMorpher): Implemented base methods

--- TextMorph/TextMorpher.cs
+++ TextMorph/TextMorpher.cs
@@ -2,12 +2,18 @@ namespace TextMorph;
 
 public class TextMorpher
 {
+    private readonly Trie _trie = new ();
+    
     public void AddMapping(string key, string value)
-        => throw new NotImplementedException();
+        => _trie.Add(key, value);
 
     public bool HasMapping(string key)
-        => throw new NotImplementedException();
-    
+    {
+        ArgumentNullException.ThrowIfNull(key);
+
+        return _trie.TryGetValue(key, out _);
+    }
+
     public string Morph(string text)
         => throw new NotImplementedException();
 }
